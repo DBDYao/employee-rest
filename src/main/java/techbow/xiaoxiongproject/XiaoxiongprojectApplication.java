708190a99c5ce5213d1e212807ad126ecbcd9f3a
DBD_Yao <yao@DBD-Yaos-MacBook-Pro.local>
2019-03-03 20:40:13 -0800

    first commit

---
+++ src/main/java/techbow/xiaoxiongproject/XiaoxiongprojectApplication.java
@@ -0,0 +1,13 @@
+package techbow.xiaoxiongproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoxiongprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoxiongprojectApplication.class, args);
+    }
+
+}
